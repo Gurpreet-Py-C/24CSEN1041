f6140f6ba802171e5c46834f365070cb8383e943
Gurpreet-Py-C <gmand@student.gitam.edu>
2026-01-01 15:01:40 +0530

    Gives largest of two given numbers

---
+++ Largest num.py
@@ -0,0 +1,8 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+if a > b:
+	print(f"{a} is larger")
+elif b > a:
+	print(f"{b} is larger")
+else:
+	print("Both are equal")
