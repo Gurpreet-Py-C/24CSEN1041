51041f74005793462c4df765ff729da65bf20514
Gurpreet-Py-C <gmand@student.gitam.edu>
2026-01-03 10:35:12 +0530

    Automatic grading system

---
+++ Grading.py
@@ -0,0 +1,12 @@
+marks = float(input("Enter marks (0-100): "))
+if marks >= 90:
+	grade = 'A'
+elif marks >= 80:
+	grade = 'B'
+elif marks >= 70:
+	grade = 'C'
+elif marks >= 60:
+	grade = 'D'
+else:
+	grade = 'F'
+print(f"Your grade is: {grade}")
