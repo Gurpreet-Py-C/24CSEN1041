352063a68a8c8e889e419b048d77ccd638be7669
Gurpreet-Py-C <gmand@student.gitam.edu>
2026-01-22 14:17:17 +0530

    Calculator for 4 basic mathematic operations (+, -, *, /)

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,16 +1,16 @@
 num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
 num2 = float(input("Enter second number: "))
-print("Select operation: +, -, *, /")
-operation = input("Enter choice: ")
-if operation == '+':
-	result = num1 + num2
-elif operation == '-':
-	result = num1 - num2
-elif operation == '*':
-	result = num1 * num2
-elif operation == '/':
-	if num2 != 0:
-		result = num1 / num2
-		else:
-		result = "Invalid Operation"
-print(f"Result: {result}")
+if op == "+":
+    print("Result:", num1 + num2)
+elif op == "-":
+    print("Result:", num1 - num2)
+elif op == "*":
+    print("Result:", num1 * num2)
+elif op == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero")
+else:
+    print("Invalid operator")
