550432f3c621adecc8b6e6e546182a06aca1f5a2
Gurpreet-Py-C <gmand@student.gitam.edu>
2026-01-28 10:41:51 +0530

    Prints odd numbers within a validated user-defined range while stopping execution at a specified value.

---
+++ ClassWork/Range-Based Odd Printer.py
@@ -0,0 +1,13 @@
+start = int(input("Enter start of range: "))
+end = int(input("Enter end of range: "))
+stop_at = int(input("Stop at: "))
+if start >= end:
+    print("Invalid range: start must be less than end.")
+elif not (start <= stop_at < end):
+    print("Invalid stop value: must be within the range.")
+else:
+    for i in range(start, end):
+        if i == stop_at:
+            break
+        if i % 2 != 0:
+            print(i)
